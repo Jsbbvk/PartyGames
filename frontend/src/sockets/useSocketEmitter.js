7020eb2f536e13cf8546fe336bbc5b1c7437ecbb
Jsbbvk <jsbbvk@gmail.com>
2021-12-31 23:43:03 -0800

    removed unnecessary files

--- frontend/src/sockets/useSocketEmitter.js
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from 'react'
-
-const useSocketEmitter = ({ socket, endpoint, values, cb }) => {
-  useEffect(() => {
-    socket.emit(endpoint, values, cb)
-  }, [])
-}
-
-export default useSocketEmitter
